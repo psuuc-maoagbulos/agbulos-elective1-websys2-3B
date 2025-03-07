a78705409eaf07a7d0f75285419a3a530510a373
psuuc-maoagbulos <93295788+psuuc-maoagbulos@users.noreply.github.com>
2025-03-07 21:29:24 +0800

    Delete websys-login,home,conn,logout,error directory

--- websys-login,home,conn,logout,error/error.php
+++
@@ -1,5 +0,0 @@
-<?php session_start();
-echo "ERROR KA INSAN";
-header ("Refresh:2;url=index.php");
-session_destroy();
-?>
--- websys-login,home,conn,logout,error/logout.php
+++
@@ -1,5 +0,0 @@
-<?php session_start();
-session_destroy();
-echo "NAKA LOG OUT KANA INSAN";
-header ("Refresh:2;url=index.php");
-?>
