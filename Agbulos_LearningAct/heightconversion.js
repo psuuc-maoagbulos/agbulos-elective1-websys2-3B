9ab551f1cb6d82776a397b8c32de3d99975d179c
psuuc-maoagbulos <93295788+psuuc-maoagbulos@users.noreply.github.com>
2025-03-07 21:23:36 +0800

    Delete Agbulos_LearningAct directory

--- Agbulos_LearningAct/heightconversion.js
+++
@@ -1,9 +0,0 @@
-console.log("heightconversion.js loaded");
-
-let heightCm = 167;
-let heightM = heightCm/100;
-
-let ConvertedHeight =`Height: ${heightCm} cm <br> 
-        is equal to ${heightM} m `;
-
-        document.getElementById("Height Conversion").innerHTML = ConvertedHeight;
