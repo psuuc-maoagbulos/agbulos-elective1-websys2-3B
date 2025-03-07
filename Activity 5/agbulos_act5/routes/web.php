3f3c197f5b1fc7958b8f598edf68a3ac2cc64527
psuuc-maoagbulos <93295788+psuuc-maoagbulos@users.noreply.github.com>
2025-03-07 21:21:34 +0800

    Delete Activity 5/agbulos_act5 directory

--- Activity 5/agbulos_act5/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MathController;
-
-Route::get('/{operation}/{num1}/{num2}/{operation2}/{num3}/{num4}', [MathController::class, 'compute']);
-
-
